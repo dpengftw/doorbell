0f13bc4994747f3f46a066004d91397bd5fc9912
David Peng <dpeng1@gmail.com>
2021-02-07 21:27:41 -0800

    Convert to email routines to python

--- doorbell.py
+++ doorbell.py
@@ -1,24 +1,111 @@
-import CHIP_IO.GPIO as GPIO
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+#import CHIP_IO.GPIO as GPIO
+import configparser
 import subprocess
 import time
 import urllib
 
+config = configparser.ConfigParser()
+config.read('config.ini')
+
 pin = "GPIO1"
-url = "<your url>"
-cypher = "<your cypher api key>"
 
-def doorbellCallback(channel):
-	r = urllib.urlopen(url + "?key=" + cypher)
-	print("hit the edge")
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-GPIO.cleanup(pin)
-GPIO.setup(pin, GPIO.IN)
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-# Add Callback for Both Edges using the add_event_detect() method
-GPIO.add_event_detect(pin, GPIO.FALLING, doorbellCallback)
+    service = build('gmail', 'v1', credentials=creds)
 
-try:
-	while (not time.sleep(5)):
-		print("watching " + pin)	
-except:
-	GPIO.cleanup(pin)
+    # Call the Gmail API
+    results = service.users().labels().list(userId='me').execute()
+    message = create_message('doorbell@peng168.com', config['default']['toemails'], 'Ding Dong', 'Ding Dong')
+
+    GPIO.cleanup(pin)
+    GPIO.setup(pin, GPIO.IN)
+
+    # Add Callback for Both Edges using the add_event_detect() method
+    GPIO.add_event_detect(pin, GPIO.FALLING, doorbell_handler(service))
+
+    try:
+      while (not time.sleep(5)):
+        print("watching " + pin)	
+    except:
+      GPIO.cleanup(pin)
+
+def doorbell_handler(service):
+  sections = config.sections()
+  subject = 'Ding Dong'
+  for i in sections:
+    message = create_message('doorbell@peng168.com', i, subject, subject)
+    send_message(service, me, message)
+
+def create_message(sender, to, subject, message_text):
+  """Create a message for an email.
+
+  Args:
+    sender: Email address of the sender.
+    to: Email address of the receiver.
+    subject: The subject of the email message.
+    message_text: The text of the email message.
+
+  Returns:
+    An object containing a base64url encoded email object.
+  """
+  message = MIMEText(message_text)
+  message['to'] = to
+  message['from'] = sender
+  message['subject'] = subject
+  return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+
+def send_message(service, user_id, message):
+  """Send an email message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    message: Message to be sent.
+
+  Returns:
+    Sent Message.
+  """
+  try:
+    message = (service.users().messages().send(userId=user_id, body=message)
+               .execute())
+    print 'Message Id: %s' % message['id']
+    return message
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+
+if __name__ == '__main__':
+    main()
